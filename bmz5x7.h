b76ec6fb7aaeb23deb8a5f4240fb0fe9901bbcae
Dicky B_Mz <50789462+bmzstore@users.noreply.github.com>
2022-11-23 16:26:33 +0700

    Add files via upload

---
+++ bmz5x7.h
@@ -0,0 +1,71 @@
+const uint8_t bmz5x7Bitmaps[] PROGMEM = {
+  0x74, 0x67, 0x5C, 0xC5, 0xC0, 0x00, 0x00, 0x00, 0x23, 0x08, 0x42, 0x11, 
+  0xC0, 0x74, 0x42, 0x22, 0x23, 0xE0, 0x74, 0x42, 0x60, 0xC5, 0xC0, 0x11, 
+  0x95, 0x2F, 0x88, 0x40, 0xFC, 0x3C, 0x10, 0xC5, 0xC0, 0x74, 0x61, 0xE8, 
+  0xC5, 0xC0, 0xF8, 0x44, 0x22, 0x10, 0x80, 0x74, 0x62, 0xE8, 0xC5, 0xC0, 
+  0x74, 0x62, 0xF0, 0xC5, 0xC0, 0x47, 0x64, 0x52, 0x69, 0x54, 0xCA, 0x47, 
+  0x9C, 0x00, 0x45, 0x99, 0x12, 0x24, 0x48, 0xBB, 0x80, 0x00, 0x00, 0x00, 
+  0x47, 0x64, 0x50, 0x28, 0x24, 0x22, 0x23, 0xBE, 0x00, 0x3C, 0xF0, 0x00, 
+  0x00, 0x00, 0x00, 0x00
+};
+
+const GFXglyph bmz5x7Glyphs[] PROGMEM = {
+  {     0,   5,  11,   0,    0,   -2 },   // 0x0 'non-printable'
+  {     8,   5,   7,   0,    0,   -2 },   // 0x1 'non-printable'
+  {    13,   5,   7,   0,    0,   -2 },   // 0x2 'non-printable'
+  {    18,   5,   7,   0,    0,   -2 },   // 0x3 'non-printable'
+  {    23,   5,   7,   0,    0,   -2 },   // 0x4 'non-printable'
+  {    28,   5,   7,   0,    0,   -2 },   // 0x5 'non-printable'
+  {    33,   5,   7,   0,    0,   -2 },   // 0x6 'non-printable'
+  {    38,   5,   7,   0,    0,   -2 },   // 0x7 'non-printable'
+  {    43,   5,   7,   0,    0,   -2 },   // 0x8 'non-printable'
+  {    48,   5,   7,   0,    0,   -2 },   // 0x9 'non-printable'
+  {    53,   9,   8,   0,    0,   -2 },   // 0xA 'non-printable'
+  {    62,   7,  10,   0,    0,   -2 },   // 0xB 'non-printable'
+  {    72,   9,   7,   0,    0,   -2 },   // 0xC 'non-printable'
+  {    81,   2,   7,   0,    0,   -2 },   // 0xD 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0xE 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0xF 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x10 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x11 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x12 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x13 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x14 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x15 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x16 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x17 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x18 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x19 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x1A 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x1B 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x1C 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x1D 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x1E 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x1F 'non-printable'
+  {     0,   0,   0,   0,    0,    0 },   // 0x20 ' '
+  {     0,   0,   0,   0,    0,    0 },   // 0x21 '!'
+  {     0,   0,   0,   0,    0,    0 },   // 0x22 '"'
+  {     0,   0,   0,   0,    0,    0 },   // 0x23 '#'
+  {     0,   0,   0,   0,    0,    0 },   // 0x24 '$'
+  {     0,   0,   0,   0,    0,    0 },   // 0x25 '%'
+  {     0,   0,   0,   0,    0,    0 },   // 0x26 '&'
+  {     0,   0,   0,   0,    0,    0 },   // 0x27 '''
+  {     0,   0,   0,   0,    0,    0 },   // 0x28 '('
+  {     0,   0,   0,   0,    0,    0 },   // 0x29 ')'
+  {     0,   0,   0,   0,    0,    0 },   // 0x2A '*'
+  {     0,   0,   0,   0,    0,    0 },   // 0x2B '+'
+  {     0,   0,   0,   0,    0,    0 },   // 0x2C ','
+  {     0,   0,   0,   0,    0,    0 },   // 0x2D '-'
+  {     0,   0,   0,   0,    0,    0 },   // 0x2E '.'
+  {     0,   0,   0,   0,    0,    0 },   // 0x2F '/'
+  {    84,   5,   5,   0,    0,    0 }    // 0x30 '0'
+};
+
+const GFXfont bmz5x7 PROGMEM =
+{
+  (uint8_t*) bmz5x7Bitmaps, 
+  (GFXglyph*) bmz5x7Glyphs, 0x0, 0x30,  6
+};
+
+
+// Approx. 46 bytes
